ecce6596773cea6a2ef2b2eb07055e81e0cd5e50
Swayam <swayamkumar226@gmail.com>
2025-07-08 20:27:15 +0530

    bug fixes

--- app/api/article/route.js
+++ app/api/article/route.js
@@ -1,5 +1,5 @@
 import { connectDB } from "@/lib/db";
-import { Article } from "@/models/Article";
+import { Article } from "@/models/ArticleModel";
 
 export async function GET() {
     await connectDB();
--- app/api/comment/route.js
+++ app/api/comment/route.js
@@ -1,5 +1,5 @@
 import { connectDB } from "@/lib/db";
-import { Comment } from "@/models/Comment";
+import { Comment } from "@/models/CommentModel";
 
 export async function GET(request) {
     await connectDB();
